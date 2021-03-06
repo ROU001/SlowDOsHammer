c2efc84d741158fa981ee69de26190d5997c966b
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-03-05 19:10:37 -0800

    Update SLOWDOSHAMMER2.py

--- SLOWDOSHAMMER2.py
+++ SLOWDOSHAMMER2.py
@@ -122,7 +122,7 @@ def httpcall(url):
 	except urllib2.HTTPError, e:
 			#print e.code
 			set_flag(1)
-			print 'Response Code 500'
+			print 'SLOWDOSHAMMER ATTACKING'
 			code=500
 	except urllib2.URLError, e:
 			#print e.reason
